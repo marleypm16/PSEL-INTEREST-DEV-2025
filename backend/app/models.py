e14d91fdea75cb104da607a7311e15c36df90d5d
marleypm16 <yarlempm16@gmail.com>
2026-01-08 11:26:03 -0300

    feat : criando modelos para o banco de dados de usuarios e times

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,5 +1,27 @@
-from sqlmodel import SQLModel
-
+from typing import List, Optional
+from sqlmodel import SQLModel,Field,Relationship
+import uuid
 # Arquivo base para criação de todos os modelos necessários
 # Serve como base para User e Team models
 # Através dele a exportação para o alembic deve ser executada
+
+class User(SQLModel, table=True):
+    __tablename__ = "users"
+    id:uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True, index=True)
+    name: str
+    team_id: Optional[uuid.UUID] = Field(default=None, foreign_key="teams.id")
+
+    team: Optional["Team"] = Relationship(back_populates="members")
+    leader_of: Optional["Team"] = Relationship(
+        sa_relationship_kwargs={"uselist": False},
+        back_populates="leader"
+    )
+
+class Team(SQLModel, table=True):
+    __tablename__ = "teams"
+    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True, index=True)
+    name: str
+    leader_id: uuid.UUID = Field(foreign_key="users.id")
+    
+    leader: Optional[User] = Relationship(back_populates="leader_of")
+    members: List[User] = Relationship(back_populates="team")
