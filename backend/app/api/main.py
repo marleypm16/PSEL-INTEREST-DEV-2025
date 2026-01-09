05e6579259ba6a21c97f03e649e7d367e8f3c40e
marleypm16 <yarlempm16@gmail.com>
2026-01-09 11:19:09 -0300

    feat : adicionando rota de times

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -2,7 +2,9 @@ from fastapi import APIRouter
 
 from app.api.routes import utils
 from app.api.routes import user_routes
+from app.api.routes import team_routes
 
 api_router = APIRouter()
 api_router.include_router(utils.router)
 api_router.include_router(user_routes.router) 
+api_router.include_router(team_routes.router)
