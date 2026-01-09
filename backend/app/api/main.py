b1d0e777a3fada8398814cd3d650cf306488afad
marleypm16 <yarlempm16@gmail.com>
2026-01-09 11:11:44 -0300

    feat : adicionando rota de usuarios ao arquivo main

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 
 from app.api.routes import utils
+from app.api.routes import user_routes
 
 api_router = APIRouter()
 api_router.include_router(utils.router)
+api_router.include_router(user_routes.router) 
