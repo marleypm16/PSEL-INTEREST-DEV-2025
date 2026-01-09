787fcce818fbe7c96fbc2f18bc7a11a35398a724
marleypm16 <yarlempm16@gmail.com>
2026-01-09 11:09:00 -0300

    feat : criando rota de usuarios

---
+++ backend/app/api/routes/user_routes.py
@@ -0,0 +1,35 @@
+from fastapi import APIRouter, HTTPException,Depends
+from app.schemas.user_schema import UserRead,UserCreate,UserUpdate
+from app.repositories.user_repository import UserRepository
+import uuid
+router = APIRouter(prefix="/users", tags=["users"])
+
+@router.get("/",response_model=UserRead)
+def get_users(user_repository: UserRepository = Depends(UserRepository.get_user_repository)):
+    return user_repository.get_all_users()
+
+@router.get("/{user_id}",response_model=UserRead)
+def get_user(user_id: uuid.UUID, user_repository: UserRepository = Depends(UserRepository.get_user_repository)):
+    user = user_repository.get_user_by_id(user_id)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
+
+@router.post("/",response_model=UserCreate)
+def create_user(user_data_create: UserCreate, user_repository: UserRepository = Depends(UserRepository.get_user_repository)):
+    return user_repository.create_user(user_data_create)
+
+@router.put("/{user_id}",response_model=UserUpdate)
+def update_user(user_id: uuid.UUID, user_data_update: UserUpdate, user_repository: UserRepository = Depends(UserRepository.get_user_repository)):
+    user = user_repository.get_user_by_id(user_id)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user_repository.update_user(user_id, user_data_update)
+
+@router.delete("/{user_id}")
+def delete_user(user_id: uuid.UUID, user_repository: UserRepository = Depends(UserRepository.get_user_repository)):
+    user = user_repository.get_user_by_id(user_id)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    user_repository.delete_user(user_id)
+    return {"detail": "User deleted successfully"}
