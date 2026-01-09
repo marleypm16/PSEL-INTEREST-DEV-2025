045c936ff017dc9444ec0e2898ecac5a25dce07f
marleypm16 <yarlempm16@gmail.com>
2026-01-09 20:47:40 -0300

    test : criando configuração do banco de dados de teste

---
+++ backend/app/tests/configTest.py
@@ -0,0 +1,30 @@
+import pytest
+from fastapi.testclient import TestClient
+from sqlmodel import Session, SQLModel, create_engine
+from sqlmodel.pool import StaticPool
+
+from app.main import app
+from app.core.db import get_session
+
+
+TEST_DATABASE_URL = "sqlite:///:memory:"
+engine = create_engine(
+    TEST_DATABASE_URL, connect_args={"check_same_thread": False}, poolclass=StaticPool)
+
+@pytest.fixture(name="session")
+def session_fixture():
+    SQLModel.metadata.create_all(engine)
+    with Session(engine) as session:
+        yield session
+    SQLModel.metadata.drop_all(engine)
+
+def client_fixture(session: Session):
+    def get_session_override():
+        return session
+
+    app.dependency_overrides[get_session] = get_session_override
+    
+    client = TestClient(app)
+    yield client
+    
+    app.dependency_overrides.clear()
