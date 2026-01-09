3e187ddaf677d33eea47a1c5e29ca60331fa2916
marleypm16 <yarlempm16@gmail.com>
2026-01-09 11:28:27 -0300

    refactor : alterando os status code

--- backend/app/api/routes/team_routes.py
+++ backend/app/api/routes/team_routes.py
@@ -4,11 +4,11 @@ import uuid
 from app.repositories.team_repository import TeamRepository
 router = APIRouter(prefix="/teams", tags=["teams"])
 
-@router.get("/",response_model=list[TeamRead])
+@router.get("/",response_model=list[TeamRead],status_code=200)
 def get_teams(team_repository: TeamRepository = Depends(TeamRepository.get_team_repository)):
     return team_repository.get_all_teams()
 
-@router.get("/{team_id}",response_model=TeamRead,description="Get a team by their ID")
+@router.get("/{team_id}",response_model=TeamRead,status_code=200,description="Get a team by their ID")
 def get_team(team_id: uuid.UUID, team_repository: TeamRepository = Depends(TeamRepository.get_team_repository)):
     team = team_repository.get_team_by_id(team_id)
     if not team:
@@ -26,7 +26,7 @@ def update_team(team_id: uuid.UUID, team_data_update: TeamUpdate, team_repositor
         raise HTTPException(status_code=404, detail="Team not found")
     return team_repository.update_team(team_id, team_data_update)
 
-@router.delete("/{team_id}")
+@router.delete("/{team_id}",status_code=204)
 def delete_team(team_id: uuid.UUID, team_repository: TeamRepository = Depends(TeamRepository.get_team_repository)):
     team = team_repository.get_team_by_id(team_id)
     if not team:
@@ -34,14 +34,14 @@ def delete_team(team_id: uuid.UUID, team_repository: TeamRepository = Depends(Te
     team_repository.delete_team(team_id)
     return {"detail": "Team deleted successfully"}
 
-@router.post("/{team_id}/membro/{user_id}")
+@router.post("/{team_id}/membro/{user_id}",status_code=201)
 def add_member_to_team(team_id: uuid.UUID, user_id: uuid.UUID, team_repository: TeamRepository = Depends(TeamRepository.get_team_repository)):
     user = team_repository.add_member_to_team(team_id, user_id)
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
     return user
 
-@router.delete("/{team_id}/membro/{user_id}")
+@router.delete("/{team_id}/membro/{user_id}" ,status_code=204)
 def remove_member_from_team(team_id: uuid.UUID, user_id: uuid.UUID, team_repository: TeamRepository = Depends(TeamRepository.get_team_repository)):
     user = team_repository.remove_member_from_team(team_id, user_id)
     if not user:
--- backend/app/api/routes/user_routes.py
+++ backend/app/api/routes/user_routes.py
@@ -4,29 +4,29 @@ from app.repositories.user_repository import UserRepository
 import uuid
 router = APIRouter(prefix="/users", tags=["users"])
 
-@router.get("/",response_model=list[UserRead])
+@router.get("/",response_model=list[UserRead],status_code=200)
 def get_users(user_repository: UserRepository = Depends(UserRepository.get_user_repository)):
     return user_repository.get_all_users()
 
-@router.get("/{user_id}",response_model=UserRead,description="Get a user by their ID")
+@router.get("/{user_id}",response_model=UserRead,status_code=200,description="Get a user by their ID")
 def get_user(user_id: uuid.UUID, user_repository: UserRepository = Depends(UserRepository.get_user_repository)):
     user = user_repository.get_user_by_id(user_id)
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
     return user
 
-@router.post("/",response_model=UserCreate)
+@router.post("/",response_model=UserCreate,status_code=201)
 def create_user(user_data_create: UserCreate, user_repository: UserRepository = Depends(UserRepository.get_user_repository)):
     return user_repository.create_user(user_data_create)
 
-@router.put("/{user_id}",response_model=UserUpdate)
+@router.put("/{user_id}",response_model=UserUpdate,status_code=200)
 def update_user(user_id: uuid.UUID, user_data_update: UserUpdate, user_repository: UserRepository = Depends(UserRepository.get_user_repository)):
     user = user_repository.get_user_by_id(user_id)
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
     return user_repository.update_user(user_id, user_data_update)
 
-@router.delete("/{user_id}")
+@router.delete("/{user_id}",status_code=204)
 def delete_user(user_id: uuid.UUID, user_repository: UserRepository = Depends(UserRepository.get_user_repository)):
     user = user_repository.get_user_by_id(user_id)
     if not user:
