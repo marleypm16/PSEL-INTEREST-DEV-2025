5c073103863798ed1a442a7fd5a46ae9ae8b3de0
marleypm16 <yarlempm16@gmail.com>
2026-01-12 20:18:08 -0300

    feat : adicionando side bar no mainlayout

--- frontend/src/layouts/MainLayout.tsx
+++ frontend/src/layouts/MainLayout.tsx
@@ -1,19 +1,18 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import Header from '../components/Header'
-import Footer from '../components/Footer'
+import { Outlet } from 'react-router-dom';
+import SideBar from '../components/sidebar/SideBar';
 
-const MainLayout: React.FC = () => {
+const MainLayout = () => {
+
+
   return (
-    <div className="app-layout">
-      <div className="stars"></div>
-      <Header />
+
+      <div className="app-layout">
+        <SideBar/>
       <main className="main-content">
         <Outlet />
       </main>
-      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default MainLayout
+export default MainLayout;
