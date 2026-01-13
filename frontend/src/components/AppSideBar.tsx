ffdae01398b5764f9bc5c761031eb27a3e0008a8
marleypm16 <yarlempm16@gmail.com>
2026-01-13 10:53:33 -0300

    feat : criação do componente de sidebar

---
+++ frontend/src/components/AppSideBar.tsx
@@ -0,0 +1,55 @@
+import { Users, UsersRound } from "lucide-react"
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "./ui/sidebar"
+import { NavLink } from "react-router-dom"
+
+// Menu items.
+const items = [
+  {
+    title: "Teams",
+    url: "/teams",
+    icon: UsersRound,
+  },
+  {
+    title: "Users",
+    url: "/users",
+    icon: Users,
+  },
+]
+
+const AppSidebar = () => {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <NavLink to={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </NavLink>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  )
+}
+
+export default AppSidebar
