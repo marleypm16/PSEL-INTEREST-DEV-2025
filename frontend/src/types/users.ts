40728285554621e804ec50eaeb087b85c1fe79d6
marleypm16 <yarlempm16@gmail.com>
2026-01-13 15:57:44 -0300

    feat : criando dto para criação de usuário

--- frontend/src/types/users.ts
+++ frontend/src/types/users.ts
@@ -1,5 +1,15 @@
 export interface User {
-    id: string;
-    name: string;
-    team_id?: string;
+  id: string;
+  name: string;
+  email: string;
+  team: string;
+  team_id: string;
+  is_active: boolean;
+}
+
+export interface CreateUserDTO {
+  name: string;
+  email: string;
+  team_id: string;
+  is_active: boolean;
 }
