03ae4e73e9a750e71caed6f59a8c001ac3377816
marleypm16 <yarlempm16@gmail.com>
2026-01-13 14:57:06 -0300

    feat : adicionando modal para criação e edição de usuarios

---
+++ frontend/src/components/usersPage/UserModal.tsx
@@ -0,0 +1,148 @@
+import { useEffect, useState } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogFooter,
+} from "../ui/dialog";
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+import { Label } from "../ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
+import { CreateUserDTO, User } from "../../types/users";
+interface UserDialogProps {
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+  user?: User;
+  onSave: (user: CreateUserDTO) => void;
+}
+
+const  UserModal = ({ open, onOpenChange, user, onSave }: UserDialogProps) => {
+  const [formData, setFormData] = useState<CreateUserDTO>({
+    name: "",
+    email: "",
+    team_id: "",
+    is_active: true,
+  });
+
+  useEffect(() => {
+    if (user) {
+      setFormData(user);
+    } else {
+      setFormData({
+        name: "",
+        email: "",
+        team_id: "",
+        is_active: true,
+      });
+    }
+  }, [user, open]);
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log("Form Data to Save:", formData);
+    onSave(formData);
+  };
+
+  return (
+    <Dialog open={open} onOpenChange={onOpenChange}>
+      <DialogContent className="sm:max-w-125">
+        <DialogHeader>
+          <DialogTitle>
+            {user ? "Editar Usuário" : "Adicionar Novo Usuário"}
+          </DialogTitle>
+        </DialogHeader>
+        <form onSubmit={handleSubmit}>
+          <div className="space-y-4 py-4">
+            <div className="space-y-2">
+              <Label htmlFor="name">Nome Completo</Label>
+              <Input
+                id="name"
+                value={formData.name}
+                onChange={(e) => setFormData({ ...formData, name: e.target.value })}
+                placeholder="Ex: João Silva"
+                required
+              />
+            </div>
+
+            <div className="space-y-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                value={formData.email}
+                onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+                placeholder="joao.silva@company.com"
+                required
+              />
+            </div>
+
+
+            <div className="grid grid-cols-2 gap-4">
+
+              <div className="space-y-2">
+                <Label htmlFor="team">Equipe</Label>
+                <Select
+                  value={formData.team_id}
+                  onValueChange={(value) => setFormData({ ...formData, team_id: value })}
+                >
+                  <SelectTrigger>
+                    <SelectValue placeholder="Selecione" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectItem value="Desenvolvimento">Desenvolvimento</SelectItem>
+                    <SelectItem value="Marketing">Marketing</SelectItem>
+                    <SelectItem value="Vendas">Vendas</SelectItem>
+                    <SelectItem value="Suporte">Suporte</SelectItem>
+                    <SelectItem value="RH">RH</SelectItem>
+                    <SelectItem value="Design">Design</SelectItem>
+                    <SelectItem value="TI">TI</SelectItem>
+                  </SelectContent>
+                </Select>
+              </div>
+            </div>
+
+            <div className="space-y-2">
+              <Label htmlFor="status">Status</Label>
+              <Select
+                value={formData.is_active ? "active" : "inactive"}
+                onValueChange={(value: "active" | "inactive") =>
+                  setFormData({ ...formData, is_active: value === "active" })
+                }
+              >
+                <SelectTrigger>
+                  <SelectValue />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="active">Ativo</SelectItem>
+                  <SelectItem value="inactive">Inativo</SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+          </div>
+
+          <DialogFooter>
+            <Button
+              type="button"
+              variant="outline"
+              onClick={() => onOpenChange(false)}
+            >
+              Cancelar
+            </Button>
+            <Button type="submit">
+              {user ? "Salvar Alterações" : "Adicionar Usuário"}
+            </Button>
+          </DialogFooter>
+        </form>
+      </DialogContent>
+    </Dialog>
+  );
+}
+export default UserModal;
