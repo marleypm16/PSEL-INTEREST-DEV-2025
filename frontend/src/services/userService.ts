79a5351b23561003dbbdc695b91c11b0bb2b7ccb
marleypm16 <yarlempm16@gmail.com>
2026-01-13 14:55:35 -0300

    feat : criação de metodos da api

---
+++ frontend/src/services/userService.ts
@@ -0,0 +1,20 @@
+import api from "../api/api";
+import { User } from "../types/users";
+export const createUser = async (userData: Omit<User, "id">): Promise<User> => {
+    const response = await api.post<User>("/users", userData);
+    return response.data;
+}
+
+export const getUsers = async (): Promise<User[]> => {
+    const response = await api.get<User[]>("/users");
+    return response.data;
+}
+
+export const updateUser = async (userId: string, userData: Omit<User, "id">): Promise<User> => {
+    const response = await api.put<User>(`/users/${userId}`, userData);
+    return response.data;
+}
+
+export const deleteUser = async (userId: string): Promise<void> => {
+    await api.delete(`/users/${userId}`);
+}
