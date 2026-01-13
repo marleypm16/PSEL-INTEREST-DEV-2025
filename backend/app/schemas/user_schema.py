2da319f7dddf4fa3f58a5f681f5ed8102cda3fcf
marleypm16 <yarlempm16@gmail.com>
2026-01-13 11:12:39 -0300

    feat : adicionando campos novos para usuarios

--- backend/app/schemas/user_schema.py
+++ backend/app/schemas/user_schema.py
@@ -3,13 +3,19 @@ from typing import Optional
 import uuid
 class UserBase(BaseModel):
     name:str = Field(..., min_length=2,description="Nome do usuário")
+    email: str = Field(..., description="Email do usuário")
+    is_active: bool = Field(default=True, description="Indica se o usuário está ativo")
 
 class UserCreate(UserBase):
     pass
 
 class UserUpdate(UserBase):
     name: Optional[str] = Field(None, min_length=2,description="Nome do usuário")
+    email: Optional[str] = Field(None, description="Email do usuário")
+    is_active: Optional[bool] = Field(None, description="Indica se o usuário está ativo")
 
 class UserRead(UserBase):
     id: uuid.UUID
+    email: str
+    is_active: bool
     team_id: Optional[uuid.UUID] = None
