5e7723fb9a15c7601a62ab811070461f79c0c9e3
marleypm16 <yarlempm16@gmail.com>
2026-01-13 17:01:00 -0300

    feat : criando dto

--- frontend/src/types/teams.ts
+++ frontend/src/types/teams.ts
@@ -3,9 +3,11 @@ import { User } from "./users";
 export interface Team {
   id: string;
   name: string;
-  leader?: {
-    id: string;
-    name: string;
-  };
+  leader: User;
   members?: User[]
+}
+
+export interface CreateTeamDTO {
+  name: string;
+  leader_id?: string;
 }
