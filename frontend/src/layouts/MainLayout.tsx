c1840a243f132f1b1afda31b8791b04ce9ecf010
marleypm16 <yarlempm16@gmail.com>
2026-01-13 16:00:29 -0300

    feat : adicionando toaster

--- frontend/src/layouts/MainLayout.tsx
+++ frontend/src/layouts/MainLayout.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from 'react-router-dom';
 import { SidebarProvider, SidebarTrigger } from '../components/ui/sidebar';
 import AppSidebar from '../components/AppSideBar';
+import { Toaster } from '../components/ui/sonner';
 const MainLayout = () => {
   return (
     <SidebarProvider>
@@ -8,6 +9,7 @@ const MainLayout = () => {
         <main className='flex-1 p-6'>
           <SidebarTrigger />
           <Outlet />
+          <Toaster/>
         </main>
     </SidebarProvider>
   
