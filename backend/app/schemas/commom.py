7717918d12c404d906eedcec2448aacd647b8824
marleypm16 <yarlempm16@gmail.com>
2026-01-14 13:15:08 -0300

    feat : adicionando validadores

--- backend/app/schemas/commom.py
+++ backend/app/schemas/commom.py
@@ -1,23 +1,54 @@
 # app/schemas/common.py
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, field_validator
 import uuid
 from typing import Optional
+import re
 
 # --- USER BASIC ---
 class UserBase(BaseModel):
-    name: str = Field(..., min_length=2, description="Nome do usuário")
-    email: str = Field(..., description="Email do usuário") # Use EmailStr é melhor
+    
+    name: str = Field(..., description="Nome do usuário")
+    email: str = Field(..., description="Email do usuário") 
+
+    @field_validator('name')
+    @classmethod
+    def validar_nome(cls, v: str):
+        v = v.strip()
+        # ADICIONEI A VALIDAÇÃO DE TAMANHO AQUI
+        if len(v) < 2:
+            raise ValueError('Nome inválido: deve ter pelo menos 2 caracteres')
+            
+        # Outras validações que já existiam
+        if v.isdigit():
+            raise ValueError('Nome inválido: não deve conter apenas números')
+        return v
+
+    @field_validator('email')
+    @classmethod
+    def validar_email(cls, v: str):
+        v = v.strip().lower()
+
+        email_regex = r'^[\w\.-]+@[\w\.-]+\.\w+$'
+        if not re.match(email_regex, v):
+            raise ValueError('Email inválido')
+        return v
 
 class UserReadSimple(UserBase):
-    """User apenas com ID, sem time aninhado"""
     id: uuid.UUID
     team_id: Optional[uuid.UUID] = None
 
 # --- TEAM BASIC ---
 class TeamBase(BaseModel):
-    name: str = Field(..., min_length=2, description="Nome do time")
-    leader_id: Optional[uuid.UUID] = Field(None, description="ID do líder")
+    name: str = Field(..., description="Nome do time")
+    leader_id: uuid.UUID = Field(None, description="ID do líder")
+
+    @field_validator('name')
+    @classmethod
+    def validar_nome_time(cls, v: str):
+        v = v.strip()
+        if len(v) < 2:
+            raise ValueError('Nome do time muito curto: informe pelo menos 2 letras')
+        return v
 
 class TeamReadSimple(TeamBase):
-    """Team apenas com ID, sem membros aninhados"""
     id: uuid.UUID
