bd69f11dcf46190356f9650d9e85530d52ffa1e0
marleypm16 <yarlempm16@gmail.com>
2026-01-14 03:37:40 -0300

    refactor: mudando tipos de respostas

--- backend/app/api/routes/team_routes.py
+++ backend/app/api/routes/team_routes.py
@@ -1,25 +1,24 @@
 from app.models import Team
-from app.schemas.user_schema import UserRead
 from fastapi import APIRouter,Depends,HTTPException
-from app.schemas.team_schema import TeamRead,TeamCreate,TeamUpdate
+from app.schemas.team_schema import TeamReadWithMembers,TeamCreate,TeamUpdate
 import uuid
 from app.repositories.team_repository import TeamRepository
 from sqlalchemy.exc import IntegrityError 
 
 router = APIRouter(prefix="/teams", tags=["teams"])
 
-@router.get("/",response_model=list[TeamRead],status_code=200)
+@router.get("/",response_model=list[TeamReadWithMembers],status_code=200)
 def get_teams(team_repository: TeamRepository = Depends(TeamRepository.get_team_repository)):
     return team_repository.get_all_teams()
 
-@router.get("/{team_id}",response_model=TeamRead,status_code=200,description="Get a team by their ID")
+@router.get("/{team_id}",response_model=TeamReadWithMembers,status_code=200,description="Get a team by their ID")
 def get_team(team_id: uuid.UUID, team_repository: TeamRepository = Depends(TeamRepository.get_team_repository)):
     team = team_repository.get_team_by_id(team_id)
     if not team:
         raise HTTPException(status_code=404, detail="Team not found")
     return team
 
-@router.post("/",response_model=TeamRead,status_code=201)
+@router.post("/",response_model=TeamReadWithMembers,status_code=201)
 def create_team(team_data_create: TeamCreate, team_repository: TeamRepository = Depends(TeamRepository.get_team_repository)):
     try:
         new_team = Team(**team_data_create.model_dump())
@@ -29,7 +28,7 @@ def create_team(team_data_create: TeamCreate, team_repository: TeamRepository = 
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
 
-@router.put("/{team_id}",response_model=TeamRead)
+@router.put("/{team_id}",response_model=TeamReadWithMembers)
 def update_team(team_id: uuid.UUID, team_data_update: TeamUpdate, team_repository: TeamRepository = Depends(TeamRepository.get_team_repository)):
     team = team_repository.get_team_by_id(team_id)
     team_data_update_dict = team_data_update.model_dump(exclude_unset=True)
@@ -45,7 +44,7 @@ def delete_team(team_id: uuid.UUID, team_repository: TeamRepository = Depends(Te
     team_repository.delete_team(team_id)
     return None
 
-@router.post("/{team_id}/member/{user_id}",status_code=201,response_model=TeamRead)
+@router.post("/{team_id}/member/{user_id}",status_code=201,response_model=TeamReadWithMembers)
 def add_member_to_team(team_id: uuid.UUID, user_id: uuid.UUID, team_repository: TeamRepository = Depends(TeamRepository.get_team_repository)):
     team = team_repository.add_member_to_team(team_id, user_id)
     if not team:
--- backend/app/api/routes/user_routes.py
+++ backend/app/api/routes/user_routes.py
@@ -1,27 +1,27 @@
 from app.models import User
 from fastapi import APIRouter, HTTPException,Depends
-from app.schemas.user_schema import UserRead,UserCreate,UserUpdate
+from app.schemas.user_schema import UserCreate,UserUpdate,UserReadWithTeam
 from app.repositories.user_repository import UserRepository
 import uuid
 router = APIRouter(prefix="/users", tags=["users"])
 
-@router.get("/",response_model=list[UserRead],status_code=200)
+@router.get("/",response_model=list[UserReadWithTeam],status_code=200)
 def get_users(user_repository: UserRepository = Depends(UserRepository.get_user_repository)):
     return user_repository.get_all_users()
 
-@router.get("/{user_id}",response_model=UserRead,status_code=200,description="Get a user by their ID")
+@router.get("/{user_id}",response_model=UserReadWithTeam,status_code=200,description="Get a user by their ID")
 def get_user(user_id: uuid.UUID, user_repository: UserRepository = Depends(UserRepository.get_user_repository)):
     user = user_repository.get_user_by_id(user_id)
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
     return user
 
-@router.post("/",response_model=UserRead,status_code=201)
+@router.post("/",response_model=UserReadWithTeam,status_code=201)
 def create_user(user_data_create: UserCreate, user_repository: UserRepository = Depends(UserRepository.get_user_repository)):
     new_user = User(**user_data_create.model_dump())
     return user_repository.create_user(new_user)
 
-@router.put("/{user_id}",response_model=UserRead,status_code=200)
+@router.put("/{user_id}",response_model=UserReadWithTeam,status_code=200)
 def update_user(user_id: uuid.UUID, user_data_update: UserUpdate, user_repository: UserRepository = Depends(UserRepository.get_user_repository)):
     user = user_repository.get_user_by_id(user_id)
     if not user:
