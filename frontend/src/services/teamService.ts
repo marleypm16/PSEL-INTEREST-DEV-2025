0680691b50a0f91f639c6542997e5b57faebc577
marleypm16 <yarlempm16@gmail.com>
2026-01-13 23:53:52 -0300

    feat : pegar time pelo id

--- frontend/src/services/teamService.ts
+++ frontend/src/services/teamService.ts
@@ -4,7 +4,10 @@ export const createTeam = async (teamData: CreateTeamDTO): Promise<Team> => {
     const response = await api.post<Team>("/teams", teamData);
     return response.data;
 }
-
+export const getTeamById = async (teamId: string): Promise<Team> => {
+    const response = await api.get<Team>(`/teams/${teamId}`);
+    return response.data;
+}
 export const getTeams = async (): Promise<Team[]> => {
     const response = await api.get<Team[]>("/teams");
     return response.data;
@@ -16,9 +19,9 @@ export const updateTeam = async (teamId: string, teamData: CreateTeamDTO): Promi
 export const deleteTeam = async (teamId: string): Promise<void> => {
     await api.delete(`/teams/${teamId}`);
 }
-
-export const transferMember = async (memberId: string, newTeamId: string): Promise<void> => {
-    await api.post(`/{${newTeamId}/member/{${memberId}`, { member_id: memberId, new_team_id: newTeamId });
+export const addMember = async (teamId: string, memberId: string): Promise<Team> => {
+    const response = await api.post<Team>(`/teams/${teamId}/member/${memberId}`);
+    return response.data;
 }
 
 export const removeMember = async (memberId: string, teamId: string): Promise<void> => {
